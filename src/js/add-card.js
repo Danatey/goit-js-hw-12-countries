e2366fbeea87d2cb5de4f91516ff0d5c63217bb1
Данатей <danatey40793@gmail.com>
2021-05-29 13:27:13 +0300

    +template,style, function add

---
+++ src/js/add-card.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://restcountries.eu/rest/v2';
+const endpoint = '/name';
+
+const fetchCountry = (searchEl) => {
+    const link = `${BASE_URL}${endpoint}/${searchEl}`;
+
+  return fetch(link)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return Promise.reject('Fetch have not worked: error' + response.status);
+    })
+    .catch(err => {
+      alert(err);
+    });
+}
+
+export default { fetchCountry };
